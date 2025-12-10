be6d26e95a807c76dceb8f25f0a8a82148c4000c
cpraz <cprazwal@gmail.com>
2025-12-10 16:49:23 +0545

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,27 @@
-import { defineConfig , loadEnv} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig((command, mode) => {
-    const env =loadEnv(mode,process.cwd(),"")
-    console.log(env.VITE_DEBUG)
-    return {
-  plugins: [react()],
-    server : {
-      ...(env.VITE_DEBUG === "true" && {
-      proxy :{
-          "/api" : {
-              target : "http://localhost:8000",
-              changeOrigin: true,
-              secure :false
+export default defineConfig(({ command, mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), ""); // empty prefix => include all
+  console.log("VITE_DEBUG:", env.VITE_DEBUG);
+
+  return {
+    plugins: [react()],
+
+    server: {
+
+      ...(env.VITE_DEBUG === "true"
+        ? {
+            proxy: {
+              "/api": {
+                target: "http://localhost:8000",
+                changeOrigin: true,
+                secure: false,
+              },
+            },
           }
-      }
-    }
-      )}
-}})
+        : {}),
+    },
+  };
+});
